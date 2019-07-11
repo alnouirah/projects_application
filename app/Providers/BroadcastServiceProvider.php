33dda4eb9bded11b7904958e9aedf38dc0ac1208
ammar <alnouirah@gmail.com>
2019-07-11 17:14:56 +0300

    fixing key length is too long error

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Schema;
 
 class BroadcastServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,8 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        
         Broadcast::routes();
 
         require base_path('routes/channels.php');
